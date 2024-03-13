381131c53d3f36e1b4c23d6f9a4702de411bdfa4
MaxM55 <mikovmi55@gmail.com>
2024-03-13 17:36:18 +0500

    Homework_4 done

---
+++ homework/maksim_mikov/Homework_4/homework_4.py
@@ -0,0 +1,20 @@
+my_dict = {'tuple': (11, 12, 13, 14, 15), 'list': [6, 7, 8, 9, 10],
+           'dict': {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5},
+           'set': {'Philadelphia Flyers', 'Washington Capitals', 'Minnesota Wild', 'Tampa Bay Lightning',
+                   'New York Rangers'}}
+
+print(my_dict['tuple'][4])
+
+my_dict['list'].append(25)
+my_dict['list'].pop(1)
+# print(my_dict['list'])
+
+my_dict['dict']['i am a tuple'] = 555
+del my_dict['dict']['two']
+# print(my_dict['dict'])
+
+my_dict['set'].add('Florida Panthers')
+my_dict['set'].remove('Minnesota Wild')
+# print(my_dict['set'])
+
+print(my_dict)
