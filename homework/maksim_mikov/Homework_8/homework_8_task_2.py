2e1c3961da766a6e49cf49cc44c1065f8fc147c0
MaxM55 <mikovmi55@gmail.com>
2024-03-29 14:05:03 +0500

    added function generator

--- homework/maksim_mikov/Homework_8/homework_8_task_2.py
+++ homework/maksim_mikov/Homework_8/homework_8_task_2.py
@@ -2,13 +2,16 @@ def fibonacci_numbers(n):
     result = 0
     next_num = 1
     for i in range(n - 1):
+        yield result
         prev = result
         result += next_num
         next_num = prev
-    return result
 
 
-print(fibonacci_numbers(5))
-print(fibonacci_numbers(200))
-print(fibonacci_numbers(1000))
-print(fibonacci_numbers(100000))
+count = 1
+for number in fibonacci_numbers(100001):
+    if count in [5, 200, 1000, 100000]:
+        print(number)
+    count += 1
+    if count > 100000:
+        break
