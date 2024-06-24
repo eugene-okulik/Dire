c964812bf6e4f0e47fed3232a65cbf62be42e1f1
MaxM55 <mikovmi55@gmail.com>
2024-06-24 16:29:36 +0500

    fix linter  mistakes. 1. upgrade pip; 2. Lesson_23. str3

--- homework/maksim_mikov/Homework_23/lesson_23.py
+++ homework/maksim_mikov/Homework_23/lesson_23.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 options = Options()
